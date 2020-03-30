b7bd9b58c68fe3df05f578dee0c64cd8ec37e813
AhmetSaitCalisir <56252214+AhmetSaitCalisir@users.noreply.github.com>
2020-03-30 16:14:43 +0300

    İlk Kurulum

--- Yazilim Proje Yonetimi/Yazilim Proje Yonetimi/Form1.Designer.cs
+++ Yazilim Proje Yonetimi/Yazilim Proje Yonetimi/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -37,15 +38,25 @@
             this.label1.Font = new System.Drawing.Font("Niagara Engraved", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(639, 103);
+            this.label1.Size = new System.Drawing.Size(681, 103);
             this.label1.TabIndex = 0;
-            this.label1.Text = "SELAM ÇUKULATAM NABER";
+            this.label1.Text = "SELAM ÇUKULATAM NABER ?";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(30, 115);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(642, 53);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Bu kısmı değiştir. Kaydet. Hatta kapat projeyi her şeyi kaydetsin bazen tam kayde" +
+    "tmiyor çünkü. Sonra push et. Bir de ben indireyim bakayım bende de değişmiş mi?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(685, 133);
+            this.ClientSize = new System.Drawing.Size(677, 180);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
