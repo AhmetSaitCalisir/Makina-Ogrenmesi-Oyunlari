ace1d3ee0c89e9846ae0d17e6b60a72eec221dae
nilguresci <54067949+nilguresci@users.noreply.github.com>
2020-03-30 19:44:54 +0300

    değiştirdim
    değiştirdim bakalım

--- Yazilim Proje Yonetimi/Yazilim Proje Yonetimi/Form1.Designer.cs
+++ Yazilim Proje Yonetimi/Yazilim Proje Yonetimi/Form1.Designer.cs
@@ -36,29 +36,32 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Niagara Engraved", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(681, 103);
+            this.label1.Size = new System.Drawing.Size(852, 128);
             this.label1.TabIndex = 0;
             this.label1.Text = "SELAM ÇUKULATAM NABER ?";
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(30, 115);
+            this.richTextBox1.Location = new System.Drawing.Point(40, 142);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(642, 53);
+            this.richTextBox1.Size = new System.Drawing.Size(855, 64);
             this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "Bu kısmı değiştir. Kaydet. Hatta kapat projeyi her şeyi kaydetsin bazen tam kayde" +
-    "tmiyor çünkü. Sonra push et. Bir de ben indireyim bakayım bende de değişmiş mi??" +
-    "";
+            this.richTextBox1.Text = "değiştidim bakalım olucak mı . bugun çok üzgünüm çünkü birileri beni çok üzüyor b" +
+    "u gidişle her gün üzgün olmaya devam\nedicem çünkü niller hep üzülür. bu günlük b" +
+    "u kadar ";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(677, 180);
+            this.ClientSize = new System.Drawing.Size(903, 222);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
