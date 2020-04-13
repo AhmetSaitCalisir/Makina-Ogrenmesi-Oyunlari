5fe3d1f038c7b5dc94b5ffe0d3cc4648c9342fc7
nilguresci <54067949+nilguresci@users.noreply.github.com>
2020-04-13 16:16:19 +0300

    Form_Alian

---
+++ Yazılım Proje Yönetimi/Yazılım Proje Yönetimi/Form_Alian.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Yazılım_Proje_Yönetimi
+{
+    partial class Form_Alian
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form_Alian
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_Alian";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_Alian";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
